a617eecfccb408febd13bb7294bde9031e5c6ac7
APPLE <apple@APPLEs-MBP.lan>
2024-05-28 11:11:49 +0530

    first commits

---
+++ src/components/about-me.tsx
@@ -0,0 +1,15 @@
+import "./about-me.css";
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p className="logo">
+        Hello About This Page.
+        <br />
+        How do you do?
+      </p>
+    </>
+  );
+}
+
+export default AboutPage;
